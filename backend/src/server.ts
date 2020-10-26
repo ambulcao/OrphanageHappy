1ff1e34e18d0cbb45718a3594952391a9e6eeb63
Bulcao <ambulcao@gmail.com>
2020-10-26 22:47:02 +0000

    Create database app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,23 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+
+import './database/connection';
 
 const app = express();
 
-app.get('/users', (resquest, response) => {
+app.use(express.json());
+
+app.post('/orphanages', (resquest, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = resquest.body;
+
   return response.json({ message: 'Hello World' });
 });
 
