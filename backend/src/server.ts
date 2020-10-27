fe1baba19ef030838aec7cdd627d0d43e2b57c97
Bulcao <ambulcao@gmail.com>
2020-10-27 21:55:26 +0000

    Abstraction code and create controllers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,24 +1,11 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/orphanages', (resquest, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  } = resquest.body;
-
-  return response.json({ message: 'Hello World' });
-});
+app.use(routes);
 
 app.listen(3333);
